e3cd18614b28715fe9762c98055b2d85afa769c7
bellyb88 <bellyb88@gmail.com>
2019-12-19 21:39:03 +0100

    wyglad

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('upload_faktura/', upload_faktura, name='upload_faktura'),
     path('faktura_list/', FakturaList.as_view(), name='faktura_list'),
 
-    path('lek_list_nazwa_a/', LekList_Nazwa_A.as_view(), name='lek_list_nazwa_a'),
+    path('', LekList_Nazwa_A.as_view(), name='lek_list_nazwa_a'),
     path('lek_list_nazwa_o/', LekList_Nazwa_O.as_view(), name='lek_list_nazwa_o'),
 
     path('lek_list_faktura_a/', LekList_Faktura_A.as_view(), name='lek_list_faktura_a'),
